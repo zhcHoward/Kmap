084906dd61f0500784c6883daab2345bce603f3d
Howard Zhu <zhc_howard@hotmail.com>
2021-06-06 22:39:31 +0800

    rewrite this project with Quine–McCluskey algorithm and petrick's method

--- Kmap.py
+++ Kmap.py
@@ -4,27 +4,20 @@
 This script defines a function to simplify boolean algebra expressions,
 inspired by Karnaugh Map.
 """
-import itertools
-from copy import deepcopy
 
 from utils import (
     Term,
-    diff_terms,
-    get_not_simplified_terms,
-    remove_repeated_sources,
-    remove_redundant_terms,
-    read_matrix,
-    make_chart,
+    find_essential_prime_implicants,
     find_prime_implicants,
-    petrick_method,
 )
 
 
 class Minterms(object):
     """ Minterms stores expressions for 1s and "don't care". """
 
-    def __init__(self, minterms=None, not_cares=None, nov=0):
-        #print(minterms)
+    def __init__(
+        self, minterms=None, not_cares=None,
+    ):
         if minterms is None:
             minterms = []
         if not_cares is None:
@@ -32,96 +25,18 @@ class Minterms(object):
 
         self.minterms = minterms
         self.not_cares = not_cares
-        self.number_of_variables = nov if nov else self.minterms[0].length
-
 
     def simplify(self):
-        minterms_old = self.not_cares + self.minterms
-        minterms_original = deepcopy(minterms_old)
-        print(minterms_old)
-        no_new_term = False
-        for idx, term in enumerate(minterms_old):
-            term.source = [idx]
-
-        # loop until no terms can be simplified
-        while not no_new_term:
-            minterms_new = []
-            no_new_term = True
-
-            print("minterms old",minterms_old)
-
-            minterms_x = deepcopy(minterms_old)
-            # look into terms two by two,
-            # and simplify them if they can be simplified
-            for term1, term2 in itertools.combinations(minterms_old, 2):
-                term = diff_terms(term1, term2)
-                
-                if term:
-                    minterms_new.append(term)
-                    no_new_term = False
-
-            print("No new term", no_new_term)
-            # add the terms that can't be simplified to new terms for next loop
-            minterms_new.extend(get_not_simplified_terms(minterms_old))
-
-            print(minterms_new)
-            print("set",set(minterms_new))
-            # remove the repeated terms
-            # and prepare for next loop
-            minterms_old = []
-
-            temp = []
-            [temp.append(x) for x in minterms_new if x not in temp]
-
-            for i in range(len(temp)):
-                print(temp[i].term, temp[i].source)
-
-            for term in temp:
-                term.flag = False
-                minterms_old.append(term)
-
-            #minterms_old = remove_repeated_sources(minterms_old,minterms_x )
-            #minterms_old = remove_redundant_terms(minterms_old, self.not_cares)
-
-        minterms_new = deepcopy(minterms_old)
-        print("copy",minterms_new)
-        for i in range(len(minterms_new)):
-            print(minterms_new[i].term, minterms_new[i].source)
-        #minterms = remove_repeated_sources(minterms_new, minterms_old)
-        #print("remove repeated sources",minterms)
-#
-        #for i in range(len(minterms)):
-        #    print(minterms[i].term, minterms[i].source)
-#
-        #x = remove_redundant_terms(minterms, self.not_cares)
-        #print("remove redundant terms",x)
-
-        chart = make_chart(minterms_new, minterms_original)
-
-        essential_prime_implicants, chart, ignore_sources, ignore_terms = find_prime_implicants(chart, minterms_new)
-
-        prime_implicants_numbers = petrick_method(chart, ignore_sources, ignore_terms)
-
-        final_result = deepcopy(essential_prime_implicants)
-
-        for i in range(len(prime_implicants_numbers)):
-            final_result.append(minterms_new[prime_implicants_numbers[i]])
-
-
-        print("essnetial", essential_prime_implicants)
-        print("num", prime_implicants_numbers)
-        self.result = final_result
-        
+        prime_implicants = find_prime_implicants(self.minterms, self.not_cares)
+        result = find_essential_prime_implicants(prime_implicants, self.minterms)
+        print(result)
 
 
 if __name__ == "__main__":
-    str_terms = ["0001","0010","1001","1011","1100","1110","1111"]
-    terms_not_care = [] #["1010", "1011", "1100", "1101", "1110", "1111"]
-    #check = read_matrix()
-    #print(check)
+    str_terms = ["0100", "1000", "1010", "1011", "1100", "1111"]
+    terms_not_care = ["1001", "1110"]
     t_minterms = [Term(term) for term in str_terms]
     not_cares = [Term(term) for term in terms_not_care]
 
-    minterms = Minterms(minterms = t_minterms)
+    minterms = Minterms(t_minterms, not_cares)
     minterms.simplify()
-    print("Final Result",minterms.result)
--- utils.py
+++ utils.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import itertools
-import math
+from collections import defaultdict
 
-class Term():
-    def __init__(self, term='', source=None, flag=False):
+
+class Term:
+    def __init__(self, term="", source=None, flag=False):
         if source is None:
-            source = []
+            source = set((int(term, 2),))
         self.term = term
         self.source = source
         self.flag = flag
         self.length = len(term)
+
+    @property
+    def ones(self):
+        """ones counts the number of '1's in the term
+
+        Returns:
+            int: the amount of '1's
+        """
+        return len(list(filter(lambda c: c == "1", self.term)))
 
     def __eq__(self, other):
         return self.term == other.term
@@ -40,254 +50,80 @@ def diff_terms(term1, term2):
                     pos = idx
 
         if diff == 1:
-            new_term = '*'.join((term1.term[:pos], term2.term[pos + 1:]))
-            new_source = term1.source + term2.source
+            new_term = "*".join((term1.term[:pos], term2.term[pos + 1 :]))
+            new_source = term1.source | term2.source
             term1.flag = True
             term2.flag = True
 
             return Term(new_term, new_source)
 
 
-def get_not_simplified_terms(terms):
-    not_simplified_terms = []
-    for term in terms:
-        if not term.flag:
-            not_simplified_terms.append(term)
-
-    return not_simplified_terms
-
-
-# remove source that appears in other terms' source list
-def remove_repeated_sources(terms, standard_terms):
-    for term1, term2 in itertools.product(standard_terms, terms):
-        if term1 != term2:
-            i = 0
-            while i < len(term2.source):
-               # print("source check ", term2.source[i],term1.source)
-                if term2.source[i] in term1.source:
-                #    print("source check popped", term2.source[i],term1.source)
-                    term2.source.pop(i)
-                    i -= 1
-                i += 1
-    return terms
-
-
-# remove terms that its source list is empty
-# or its source list only contains indexes from "don't care"
-def remove_redundant_terms(terms, not_cares):
-   # print("d")
-   # print(terms)
-   # print(set(not_cares))
-    i = 0
-    while i < len(terms):
-        case1 = terms[i].source
-    #    print("te")
-    #    print(set(terms[i].source))
-    #    print(range(len(set(not_cares))))
-        case2 = set(terms[i].source).issubset(range(len(set(not_cares))))
-        
-       ## case2 = False
-        #if(all(terms[i].source for x in not_cares)):
-         #   case2 = True
-
-        if not case1 or case2:
-            terms.pop(i)
-            i -= 1
-        i += 1
-
-     #   print(terms)
-
-    return terms
-
-def make_chart(simplified_terms,minterms):
-
-    chart = [ [0 for x in range(len(minterms))] for x in range(len(simplified_terms))]
-
-    print("simplified term", simplified_terms)
-
-    print("minterm for chart", minterms)
-
-    for i in range(len(simplified_terms)):
-        for j in range(len(minterms)):
-           #print("minterm check", minterms[j])
-            if j in simplified_terms[i].source:
-                chart[i][j] = 1
-
-    print("chart", chart)
-
-    return chart
-
-def find_prime_implicants(chart, simplified_terms):
+def find_prime_implicants(minterms, not_cares):
+    table = defaultdict(set)
+    for term in minterms + not_cares:
+        table[term.ones].add(term)
 
     prime_implicants = []
+    new_implicants = True
+    while new_implicants:
+        new_implicants = False
+        new_table = defaultdict(set)
+        for key in sorted(table.keys()):
+            # print(f"key == {key}")
+            terms1 = table[key]
+            terms2 = table[key + 1]
+            if terms2:
+                for t1, t2 in itertools.product(terms1, terms2):
+                    new_term = diff_terms(t1, t2)
+                    # print(f"{t1} + {t2} = {new_term}")
+                    if not new_term:
+                        continue
 
-    print("chart dim",range(len(chart[0])),range(len(chart)) )
+                    new_table[key].add(new_term)
+                    new_implicants = True
 
-    change = 1
-    ignore_sources = []
-    ignore_terms   = []
+            for term in terms1:
+                if not term.flag:
+                    # print(f"{term} become prime implicant")
+                    prime_implicants.append(term)
 
-    while change and (len(chart) - len(ignore_terms)) > 1:
-        change = 0
-        for i in range(len(chart[0])):
-            print("chart dimension",range(len(chart[0])),range(len(chart)) )
+        table = new_table
 
-            if i not in ignore_sources:
-                print("source not covered", i)
-                count = 0
-                pos   = -1
-                for j in range(len(chart)):
-                    if j not in ignore_terms:
-                        if chart[j][i] == 1:
-                            count += 1
-                            pos = j
-                if count == 1:
-                    prime_implicants.append(simplified_terms[pos])
-                    change = 1
-                    break
-            else:
-                print("source covered", i)
-
-        if change:
-            print("pos", pos, simplified_terms[pos], simplified_terms[pos].source)
-            ignore_terms.append(int(pos))
-            print("ignorew terms",ignore_terms)
-
-            ignore_sources = ignore_sources + simplified_terms[pos].source
-
-            print("ignore_sources", ignore_sources)
-        
+    return prime_implicants
 
 
-    print("prime", prime_implicants)
+def find_essential_prime_implicants(prime_implicants, minterms):
+    chart = {}
+    for source in itertools.chain.from_iterable((t.source for t in minterms)):
+        chart[source] = set()
 
-    prime_implicants = list(set(prime_implicants))
-    
-    print("prime", prime_implicants)
+    for idx, implicant in enumerate(prime_implicants):
+        for source in implicant.source:
+            if source not in chart:
+                continue
 
-    return prime_implicants, chart, ignore_sources, ignore_terms
+            chart[source].add(idx)
+
+    sop = None
+    for products in chart.values():
+        sop = multiply(sop, products)
+
+    min = 9999
+    ids = set()
+    for p in sop:
+        length = len(p)
+        if length < min:
+            min = length
+            ids = p
+
+    return [prime_implicants[i] for i in ids]
 
 
-#multiply two terms (ex. (p1 + p2)(p1+p4+p5) )..it returns the product
-def multiplication(list1, list2):
-    list_result = []
-    #if empty
-    if len(list1) == 0 and len(list2)== 0:
-        return list_result
-    #if one is empty
-    elif len(list1)==0:
-        return list2
-    #if another is empty
-    elif len(list2)==0:
-        return list1
-
-    #both not empty
+def multiply(result, product):
+    if not result:
+        return set((frozenset((p,)) for p in product))
     else:
-        for i in list1:
-            for j in list2:
-                #if two term same
-                if i == j:
-                    #list_result.append(sorted(i))
-                    list_result.append(i)
-                else:
-                    #list_result.append(sorted(list(set(i+j))))
-                    list_result.append(list(set(i+j)))
-
-        #sort and remove redundant lists and return this list
-        list_result.sort()
-        return list(list_result for list_result,_ in itertools.groupby(list_result))
-
-def petrick_method(chart, ignore_sources, ignore_terms):
-
-    p = []
-    p_sources =[]
-
-    print("chart", chart)
-
-    for i in range(len(chart[0])):
-        
-        if i not in ignore_sources:
-
-            p_sources.append(i)
-
-            temp = [] 
-
-            for j in range(len(chart)):
-
-                if (j not in ignore_terms) and chart[j][i] == 1:
-
-                    temp.append([j])
-
-            if temp != []:
-                p.append(temp)
-
-    print("p", p, p_sources)        
-
-    for i in range(len(p)-1):
-        p[i+1] = multiplication(p[i],p[i+1])
-        #print("Px", P)
-
-    print("p", p)
-    P = sorted(p[len(p)-1],key=len)
-    print("P", P)
-    final = []
-    #find the terms with min length = this is the one with lowest cost (optimized result)
-    min=len(P[0])
-    for i in P:
-        if len(i) == min:
-            final.append(i)
-            break
-        else:
-            break
-    print("final", final[0])
-
-    return final[0]
-
-
-def read_matrix():
-    x=0 
-    file=open("table.txt","r")#reading input from file
-    input1=file.readlines()
-    countOnes={}
-    flaggedRows={k: 0 for k in range(len(input1))}
-
-    input1 = [i.strip() for i in input1]
-   # for i in range(len(input1)) :
-       # if (i == len(input1) - 1):
-       #     input1[i]=list(input1[i][:])  #parsing data in required format
-       #     size=len(input1[i])
-       #     print(i,"2")
-    
-       # else:
-       #     input1[i]=list(input1[i][:-1])  #parsing data in required format
-       #     size=len(input1[i])
-       #     print(i,"3")
-    #   if(input1[]) 
-
-    for i in range(len(input1)) :
-        input1[i]=list(input1[i][:])  #parsing data in required format
-        size=len(input1[i])
-
-    input2 = []
-
-    nov = int(2*math.log2(len(input1[i])))
-
-    x = int(2**(nov/2))
-
-    print("nov",nov)
-    print("x",x)
-
-    for i in range(x):
-        for j in range(x):
-            if(input1[i][j]=='1'):
-                print(bin(i))
-                cube = str(bin(i*x + j).replace("0b","").zfill(nov))
-                print("cube",cube)
-                input2.append(cube)
-
-    print(input2)
-
-
-
-
-    return input2
+        new_result = set()
+        for a, b in itertools.product(result, product):
+            new_result.add(a | set((b,)))
+        return new_result
